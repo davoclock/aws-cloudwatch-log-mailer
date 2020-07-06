ea643f30b0a852e63dd4fa2ce47acc2a51b0a532
davoclock <67791912+davoclock@users.noreply.github.com>
2020-07-05 21:25:40 -0400

    Create logmailer.py

---
+++ logmailer.py
@@ -0,0 +1,31 @@
+import json
+import boto3
+import gzip
+import base64
+import os
+
+sns_client = boto3.client('sns')
+
+def lambda_handler(event, context):
+    # TODO implement
+    decoded_event = json.loads(gzip.decompress(base64.b64decode(event['awslogs']['data'])))
+    print(decoded_event)
+    
+    body = '''
+    LogGroup: {loggroup}
+    Logstream: {logstream}
+    Filter Match: {filtermatch}
+    '''.format(
+        loggroup=decoded_event['logGroup'],
+        logstream=decoded_event['logStream'],
+        filtermatch=decoded_event['logEvents'][0]['message'],
+    )
+    
+    def send_message(body):
+        sns = sns_client.publish(
+            TopicArn = os.environ.get('SNS_TOPIC_ARN'),
+            Message = body,
+        )
+    
+    send_message(body)
+            
